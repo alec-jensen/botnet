757d7c38dcbb1ec73602b863ac584d17847c2ac1
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2024-04-22 21:45:34 -0500

    fix some stuff

--- agent/agent.nimble
+++ agent/agent.nimble
@@ -11,3 +11,4 @@ bin           = @["agent"]
 # Dependencies
 
 requires "nim >= 2.0.0"
+requires "ws >= 0.5.0"
--- dashboard/src/consts.js
+++ dashboard/src/consts.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://api.botnet.alecj.tk';
+export const API_URL = 'http://localhost:8000';
